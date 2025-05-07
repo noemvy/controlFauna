e696191bed278bf72392dd3835a919209fe243f7
noemvy <nvrm726@gmail.com>
2025-05-07 14:05:05 -0500

    Edit: Insertando logo

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,8 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandLogo(asset('images/logo-tocumen.png'))
+            ->brandLogoHeight('70px')
             ->colors([
                 'primary' => Color::Amber,
             ])
