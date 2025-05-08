efa275d37fbbda5c574e9106641ec89f87e1f48e
noemvy <nvrm726@gmail.com>
2025-05-08 08:17:06 -0500

    Edit: Cambios en los campos

--- app/Filament/Resources/AerodromoResource.php
+++ app/Filament/Resources/AerodromoResource.php
@@ -86,7 +86,10 @@ class AerodromoResource extends Resource
                     ]),
             ])
             ->actions([
+
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
