77578858825ed899759e0d15d49ef3764245111c
noemvy <nvrm726@gmail.com>
2025-05-09 10:25:02 -0500

    Edit: Campos para los seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,7 +27,7 @@ class DatabaseSeeder extends Seeder
         AccionesSeeder::class,
         AtractivosSeeder::class,
         GrupoSeeder::class,
-        CatalogoInventarioSeeder::class,
+        CatalogoInventariosSeeder::class,
 
     ]);
 }
